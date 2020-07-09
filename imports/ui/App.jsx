498e6a207d27fb253d0895283e8d276dfc7a13d8
srchach <gsikmo@gmail.com>
2020-07-09 13:47:10 -0500

    BUILD: Starting Meteor project

---
+++ imports/ui/App.jsx
@@ -0,0 +1,24 @@
+/** React */
+import React from 'react';
+
+/** Components */
+import { Hello } from './Hello.jsx';
+import { Info } from './Info.jsx';
+import { Task } from './Task.jsx'
+
+const tasks = [
+  {_id: 1, text: 'First Task'},
+  {_id: 2, text: 'Second Task'},
+  {_id: 3, text: 'Third Task'},
+]
+
+
+
+export const App = () => (
+  <div>
+    <h1>Welcome to Meteor!</h1>
+    <Hello/>
+    <Info/>
+    { tasks.map(task => <Task key={ task._id } task={ task }/>) }
+  </div>
+);
