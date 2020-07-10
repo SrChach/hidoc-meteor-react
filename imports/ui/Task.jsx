6442bccd962b5e2ea503892cef4fe0aa8ad5ac7f
srchach <gsikmo@gmail.com>
2020-07-10 00:46:14 -0500

    FEATURE: Communication between components. Update and delete. Reactivity

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
 
-export const Task = ({ task }) => {
-    return <li>{ task }</li>
+export const Task = ({ task, onCheckboxClick, onDeleteClick }) => {
+    return (
+        <li>
+            <button onClick={ () => onDeleteClick(task) }>&times;</button>
+            <input
+                type="checkbox"
+                checked={ Boolean(task.isChecked) }
+                onClick={ () => onCheckboxClick(task) }
+                readOnly
+            />
+            <span>{ task.task }</span>
+        </li>
+    )
 }
